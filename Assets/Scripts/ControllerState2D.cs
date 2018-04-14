588b109b95ba3494c58ff797e2ef66287f5cd515
davidson2727 <Davidson27@gmail.com>
2018-04-14 01:19:45 -0400

    Diagonal movement added, tile prefabs created

---
+++ Assets/Scripts/ControllerState2D.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerState2D
+{
+	public bool IsCollidingRight {get; set;}
+	public bool IsCollidingLeft {get; set;}
+	public bool IsCollidingAbove {get; set;}
+	public bool IsCollidingBelow {get; set;}
+	public bool IsMovingDownSlope {get; set;}
+	public bool IsMovingUpSlope {get; set;}
+	public bool IsGrounded{get{return IsCollidingBelow;} }
+	public float SlopeAngle {get;set;}
+
+	public bool HasCollisions {get {return IsCollidingRight||IsCollidingLeft||IsCollidingAbove||IsCollidingBelow; } }
+
+	public void Reset()
+	{
+		IsCollidingRight=
+		IsCollidingLeft=
+		IsCollidingAbove=
+		IsCollidingBelow=
+		IsMovingUpSlope=
+		IsMovingDownSlope=false;
+
+		SlopeAngle = 0;
+	}
+	public override string ToString()
+	{
+		return string.Format("(Controller: r: {0} l: {1} a: {2} b: {3} downSlope: {4} upSlope: {5} angle: {6})",
+		IsCollidingRight,
+		IsCollidingLeft,
+		IsCollidingAbove,
+		IsCollidingBelow,
+		IsMovingDownSlope,
+		IsMovingUpSlope,
+		SlopeAngle);
+	}
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Rigidbody2D enemy;
+	BoxCollider2D enemyBox;
+	public bool facingRight;
+	public float horizontal;
+	public int enemyHealth;
+
+
+	void Start () {
+
+		horizontal = -5f;
+
+		enemy = GetComponent<Rigidbody2D>();
+		enemyBox = GetComponent<BoxCollider2D>();
+		enemyHealth = 2;
+
+
+	}
+	void FixedUpdate () {
+		//OnCollisionEnter2D(enemyBox);
+		enemy.velocity = new Vector2(horizontal,0);
+		if(enemyHealth<=0){
+			Destroy(gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D coll){
+
+		if(coll.gameObject.tag=="Player"){
+      enemy.velocity = new Vector2(-horizontal,10);
+			//enemy.velocity = new Vector2(horizontal*=-1,0);
+			//Debug.Log(horizontal);
+			//enemyHealth -=1;
+			//enemy.velocity = new Vector2(-horizontal,0);
+    }
+		if(coll.gameObject.tag=="Obstacle"){
+      flip();
+			horizontal*=-1;
+    }
+
+	}
+
+	void flip(){
+		facingRight=!facingRight;
+		Vector2 scale = transform.localScale;
+		scale.x *=-1;
+		transform.localScale = scale;
+	}
+
+}
